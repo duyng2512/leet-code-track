88a27594aa0a9251a3d07b93cafa3ed010e6150d
duyntc <duyng2512@gmail.com>
2022-09-10 20:51:26 +0700

    Add two sum and test case

--- code/src/main/java/org/dng/code/blind75/array/TwoSum.java
+++ code/src/main/java/org/dng/code/blind75/array/TwoSum.java
@@ -1,2 +1,28 @@
-package org.dng.code.blind75.array;public class TwoSum {
+package org.dng.code.blind75.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    /**
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> tempMap = new HashMap<Integer, Integer>();
+        tempMap.put(nums[0], 0);
+        for (int i = 1; i < nums.length; i++) {
+	  if (!tempMap.containsKey(nums[i])) tempMap.put(nums[i], i);
+	  if (tempMap.containsKey(target - nums[i])){
+	      return new int[]{tempMap.get(target - nums[i]), i};
+	  }
+        }
+        return new int[]{};
+    }
+
+    public static int[] testTwoSum(int[] nums, int target) {
+        return new TwoSum().twoSum(nums, target);
+    }
 }
--- code/src/test/java/org/dng/code/blind75/array/TwoSumTest.java
+++ code/src/test/java/org/dng/code/blind75/array/TwoSumTest.java
@@ -1,4 +1,23 @@
-import static org.junit.Assert.*;
+package org.dng.code.blind75.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class TwoSumTest {
-  
+
+    @Test
+    public void twoSum() {
+        int[][] inputArray = new int[][]{ {2,7,11,15}, {3,2,4}, {3,3}};
+        int[] inputTarget = new int[]{9, 6, 6};
+        int[][] expectArray = new int[][]{{0,1}, {1,2}, {0,1}};
+
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("Current [%d] \n", i);
+            Assert.assertArrayEquals(expectArray[i],
+                                    TwoSum.testTwoSum(inputArray[i],
+                                                    inputTarget[i]));
+        }
+    }
 }
