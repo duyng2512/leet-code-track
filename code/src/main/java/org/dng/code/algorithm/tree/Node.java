c5985d373cd99771e98febe0d94b5bfbe751d901
duyntc <duyntc@vng.com.vn>
2022-09-21 23:33:23 +0700

    Add new array problems

---
+++ code/src/main/java/org/dng/code/algorithm/tree/Node.java
@@ -0,0 +1,11 @@
+package org.dng.code.algorithm.tree;
+
+public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
---
+++ code/src/main/java/org/dng/code/blind75/array/ProductOfAllExceptSelf.java
@@ -0,0 +1,32 @@
+package org.dng.code.blind75.array;
+
+import lombok.Builder;
+
+import java.util.Arrays;
+
+@Builder
+public class ProductOfAllExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int[] leftProduct = new int[nums.length];
+        int[] rightProduct = new int[nums.length];
+        leftProduct[0] = 1;
+        rightProduct[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            leftProduct[i] = leftProduct[i-1] * nums[i-1];
+            rightProduct[i] = rightProduct[i-1] * nums[nums.length - i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = leftProduct[i] * rightProduct[nums.length-i-1];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(ProductOfAllExceptSelf.builder().build().productExceptSelf(new int[]{2, 3, 5, 1})));
+    }
+
+}
