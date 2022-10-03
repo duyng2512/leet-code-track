8308033040e6cd69935df2b720950d3de93bebbb
duyntc <duyntc@vng.com.vn>
2022-10-03 12:39:32 +0700

    Update Work

---
+++ code/src/main/java/org/dng/code/grooking/sliding_window/LongestSubStrWithSameLettersAfterReplacement.java
@@ -0,0 +1,59 @@
+package org.dng.code.grooking.sliding_window;
+
+
+import lombok.Builder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Builder
+public class LongestSubStrWithSameLettersAfterReplacement {
+
+    /*
+        Problem Statement
+        Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’
+        letters with any letter, find the length of the longest substring having the same letters
+        after replacement.
+
+        Example 1:
+
+        Input: String="aabm n", k=2
+        Output: 5
+        Explanation: Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+
+        Example 2:
+
+        Input: String="bbcb", k=1
+        Output: 4
+        Explanation: Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+
+        Example 3:
+
+        Input: String="aabbbcccdef", k=1
+        Output: 3
+     */
+
+    public int characterReplacement(String s, int k) {
+        	int maxLen = 0, windowStart = 0, windowEnd;
+	int maxCount = 0;
+
+	Map<Character, Integer> map = new HashMap<>();
+        	for (windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+		  map.merge(s.charAt(windowEnd), 1, Integer::sum);
+		  maxCount = Math.max(maxCount, map.get(s.charAt(windowEnd)));
+
+		  if (windowEnd - windowStart - maxCount + 1 > k) {
+		      map.put(s.charAt(windowStart), map.get(s.charAt(windowStart)) - 1);
+		      windowStart ++;
+		  }
+	    maxLen = Math.max(windowEnd - windowStart + 1, maxLen);
+        	}
+	return maxLen;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(LongestSubStrWithSameLettersAfterReplacement.builder().build().characterReplacement("abccde", 2));
+    }
+
+}
