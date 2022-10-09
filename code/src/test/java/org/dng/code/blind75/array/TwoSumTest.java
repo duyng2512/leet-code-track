2f66479f96f4f0094fd5d06be5b62ab4226b5fe2
duyntc <duyntc@vng.com.vn>
2022-10-09 13:53:56 +0700

    Update Test class

--- code/src/test/java/org/dng/code/blind75/array/TwoSumTest.java
+++ code/src/test/java/org/dng/code/blind75/array/TwoSumTest.java
@@ -9,15 +9,5 @@ public class TwoSumTest {
 
     @Test
     public void twoSum() {
-        int[][] inputArray = new int[][]{ {2,7,11,15}, {3,2,4}, {3,3}};
-        int[] inputTarget = new int[]{9, 6, 6};
-        int[][] expectArray = new int[][]{{0,1}, {1,2}, {0,1}};
-
-        for (int i = 0; i < 3; i++) {
-            System.out.printf("Current [%d] \n", i);
-            Assert.assertArrayEquals(expectArray[i],
-                                    TwoSum.testTwoSum(inputArray[i],
-                                                    inputTarget[i]));
-        }
     }
 }
