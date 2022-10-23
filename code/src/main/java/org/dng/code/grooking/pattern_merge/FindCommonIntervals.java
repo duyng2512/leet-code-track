0f0303e6ccf1119e839086ed69c422770aefbae5
duyntc <duyntc@vng.com.vn>
2022-10-23 15:56:09 +0700

    Update work

--- code/src/main/java/org/dng/code/grooking/pattern_merge/FindCommonIntervals.java
+++ code/src/main/java/org/dng/code/grooking/pattern_merge/FindCommonIntervals.java
@@ -1,5 +1,12 @@
 package org.dng.code.grooking.pattern_merge;
 
+import lombok.Builder;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@Builder
 public class FindCommonIntervals {
 
      /*
@@ -19,5 +26,33 @@ public class FindCommonIntervals {
 	Output: [5, 7], [9, 10]
 	Explanation: The output list contains the common intervals between the two lists.
       */
-
+     
+     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+	List<int[]> intersectionResult = new LinkedList<>();
+	int first = 0;
+	int second = 0;
+	while (first < firstList.length && second < secondList.length) {
+	     int low = Math.max(firstList[first][0], secondList[second][0]);
+	     int high = Math.min(firstList[first][1], secondList[second][1]);
+	     
+	     if (low <= high) intersectionResult.add(new int[]{low, high});
+	
+	     if (firstList[first][1] > secondList[second][1]) second ++;
+	     else first ++;
+	}
+	return intersectionResult.toArray(new int[intersectionResult.size()][]);
+     }
+     
+     public static void main(String[] args) {
+	FindCommonIntervals intervals =  FindCommonIntervals.builder().build();
+	
+	// Test case 1
+	int[][] arr1 = new int[][]{{1, 3}, {5, 6}, {7, 9}};
+	int[][] arr2 = new int[][]{{2, 3}, {5, 7} };
+	int[][] result = intervals.intervalIntersection(arr1, arr2);
+ 
+	System.out.println(Arrays.deepToString(result));
+     }
+     
 }
+
