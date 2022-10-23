46eddb2a77457d5076b010a75ec5d06d0a9695a2
duyntc <duyntc@vng.com.vn>
2022-10-23 14:26:44 +0700

    Update work

---
+++ code/src/main/java/org/dng/code/grooking/slow_fast_pointer/IntersectionOfTwoLinkedList.java
@@ -0,0 +1,42 @@
+package org.dng.code.grooking.slow_fast_pointer;
+
+import org.dng.code.grooking.slow_fast_pointer.common.ListNode;
+
+public class IntersectionOfTwoLinkedList {
+     
+     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+	ListNode run = headA;
+	int lenA = 0;
+	// Find length of A
+	while (run != null) {
+	     lenA++;
+	     run = run.next;
+	}
+ 
+	run = headB;
+	int lenB = 0;
+	// Find length of A
+	while (run != null) {
+	     lenB++;
+	     run = run.next;
+	}
+	
+	int offset = Math.abs(lenA - lenB);
+	ListNode runA = headA;
+	ListNode runB = headB;
+ 
+	while (offset > 0) {
+	     offset--;
+	     if (lenA > lenB) runA = runA.next;
+	     if (lenB > lenA) runB = runB.next;
+	};
+	
+	while (runA != null && runB != null) {
+	     if(runA.equals(runB)) return runA;
+	     runA = runA.next;
+	     runB = runB.next;
+	}
+	return null;
+     }
+     
+}
