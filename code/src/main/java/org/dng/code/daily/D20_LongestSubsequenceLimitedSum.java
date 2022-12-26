5a729bee38d62f5425055bb0b9082ad5d7a5f9a3
duyntc <duyntc@vng.com.vn>
2022-12-26 18:55:37 +0700

    Just a stub

---
+++ code/src/main/java/org/dng/code/daily/D20_LongestSubsequenceLimitedSum.java
@@ -0,0 +1,36 @@
+package org.dng.code.daily;
+
+import java.util.Arrays;
+
+/**
+ * @author duyntc 12/2022
+ */
+public class D20_LongestSubsequenceLimitedSum {
+     
+     /**
+      * @desc: https://leetcode.com/problems/longest-subsequence-with-limited-sum/
+      */
+     
+     // Input: nums = [4,5,2,1], queries = [3,10,21]
+     // Output: [2,3,4]
+     static public int[] answerQueries(int[] nums, int[] queries) {
+	
+	int Q = queries.length;
+	int[] answer = new int[queries.length];
+	long[] total = new long[queries.length];
+	
+	for (int num : nums) {
+	     for (int j = 0; j < Q; j++) {
+		if ((total[j] + num) <= queries[j]) {
+		     total[j] += num;
+		     answer[j] += 1;
+		}
+	     }
+	}
+	return answer;
+     }
+     
+     public static void main(String[] args) {
+	System.out.println(Arrays.toString(answerQueries(new int[]{4, 5, 2, 1}, new int[]{3, 10, 21})));
+     }
+}
