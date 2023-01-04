331dfe1ae6c2ce23f964825c86cdc98be6399589
duyntc <duyntc@vng.com.vn>
2023-01-04 23:38:39 +0700

    Add code

---
+++ code/src/main/java/org/dng/code/grooking/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package org.dng.code.grooking.sliding_window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duyntc 01/2023
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+     
+     public int lengthOfLongestSubstring(String s) {
+	int start = 0, end;
+	int max = 0;
+	Map<Character, Integer> map = new HashMap<>();
+	
+	for (end = 0; end < s.length(); end++) {
+	     
+	     // If already in a map cut this part from start to end
+	     if (map.containsKey(s.charAt(end))) {
+		// This already in map
+		start = Math.max(start, map.get(s.charAt(end)) + 1);
+	     }
+	     map.put(s.charAt(end), end);
+	     max = Math.max(max, end - start + 1);
+	}
+	return max;
+     }
+     
+}
---
+++ code/src/main/java/org/dng/code/grooking/sliding_window/SplitStringBalancedStrings.java
@@ -0,0 +1,41 @@
+package org.dng.code.grooking.sliding_window;
+
+/**
+ * @author duyntc 01/2023
+ */
+public class SplitStringBalancedStrings {
+     
+     /*
+	Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+ 
+	Given a balanced string s, split it into some number of substrings such that:
+	
+	Each substring is balanced.
+	Return the maximum number of balanced strings you can obtain.
+	
+      */
+     
+     static public int balancedStringSplit(String s) {
+	
+	int countR = 0;
+	int countL = 0;
+	int total = 0;
+	
+	for (int i = 0; i < s.length(); i++) {
+	     if (s.charAt(i) == 'R') countR++;
+	     else countL++;
+	     
+	     if (countL == countR) {
+		total++;
+		countL++;
+		countR++;
+	     }
+	}
+	return total;
+     }
+     
+     public static void main(String[] args) {
+	System.out.println(balancedStringSplit("RLRRLLRLRL"));
+	System.out.println(balancedStringSplit("RLRRRLLRLL"));
+     }
+}
