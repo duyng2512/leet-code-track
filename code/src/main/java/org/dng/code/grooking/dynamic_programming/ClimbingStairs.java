5a07acc0b91be024de98180d6d705ce7e27cd229
duyntc <duyntc@vng.com.vn>
2023-01-08 12:18:07 +0700

    Remove some thing

---
+++ code/src/main/java/org/dng/code/grooking/dynamic_programming/ClimbingStairs.java
@@ -0,0 +1,73 @@
+package org.dng.code.grooking.dynamic_programming;
+
+/**
+ * @author duyntc 01/2023
+ */
+public class ClimbingStairs {
+     
+     /**
+      * You are climbing a staircase. It takes n steps to reach the top.
+      * <p>
+      * Each time you can either climb 1 or 2 steps. In how many distinct
+      * ways can you climb to the top?
+      * <p>
+      * <p>
+      * <p>
+      * Example 1:
+      * <p>
+      * Input: n = 2
+      * Output: 2
+      * Explanation: There are two ways to climb to the top.
+      * 1. 1 step + 1 step
+      * 2. 2 steps
+      * Example 2:
+      * <p>
+      * Input: n = 3
+      * Output: 3
+      * Explanation: There are three ways to climb to the top.
+      * 1. 1 step + 1 step + 1 step
+      * 2. 1 step + 2 steps
+      * 3. 2 steps + 1 step
+      */
+     
+     static public int climbStairs(int n) {
+	int[] memo = new int[n + 1];
+	for (int i = 0; i < n + 1; i++) {
+	     memo[i] = -1;
+	}
+	return runRecursive(n, memo);
+     }
+     
+     static public int runRecursive(int n, int[] memo) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	
+	if (memo[n] != -1) {
+	     return memo[n];
+	}
+	
+	memo[n] = runRecursive(n - 1, memo) + runRecursive(n - 2, memo);
+	
+	return memo[n];
+     }
+     
+     static public int runArray(int n) {
+	int[] arr = new int[n + 1];
+	
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	
+	for (int i = 3; i < n + 1; i++) {
+	     arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	
+	return arr[n];
+     }
+     
+     public static void main(String[] args) {
+	System.out.println(climbStairs(10)); // 89
+	System.out.println(runArray(10)); // 89
+     }
+}
---
+++ code/src/main/java/org/dng/code/grooking/dynamic_programming/HouseRobber.java
@@ -0,0 +1,58 @@
+package org.dng.code.grooking.dynamic_programming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duyntc 01/2023
+ */
+public class HouseRobber {
+     
+     static public int rob(int[] nums) {
+	Map<Integer, Integer> map = new HashMap<>();
+	return run(map, nums, 0);
+     }
+     
+     static public int run(Map<Integer, Integer> map, int[] nums, int index) {
+	// When exceed just return base case = 0  for computation
+	if (index >= nums.length) {
+	     return 0;
+	}
+	
+	if (map.containsKey(index)) {
+	     return map.get(index);
+	}
+	
+	// Because we can not take adjacent house, so we have to
+	// choose take the next-next house or not take it at all
+	
+	int choose = run(map, nums, index + 2) + nums[index];
+	int notChoose = run(map, nums, index + 1);
+	
+	map.put(index, Math.max(choose, notChoose));
+	
+	return map.get(index);
+     }
+     
+     static public int runArray(int[] nums) {
+	int N = nums.length;
+	int[] result = new int[N + 1];
+	
+	// Base case
+	result[N] = 0;
+	result[N - 1] = nums[N - 1];
+	
+	for (int i = N - 2; i >= 0; i--) {
+	     result[i] = Math.max(nums[i] + result[i + 2], result[i + 1]);
+	}
+	
+	return result[0];
+     }
+     
+     public static void main(String[] args) {
+	System.out.println(rob(new int[]{1, 2, 3, 1})); // 4
+	System.out.println(rob(new int[]{2, 7, 9, 3, 1})); // 12
+	System.out.println(runArray(new int[]{1, 2, 3, 1})); // 4
+	System.out.println(runArray(new int[]{2, 7, 9, 3, 1})); // 12
+     }
+}
