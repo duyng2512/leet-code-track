22e6f85415c7c4bd8e7a94f29d325607da2a7add
duyntc <duyntc@vng.com.vn>
2023-02-15 00:31:07 +0700

    just a stub

---
+++ code/src/main/java/org/dng/code/grooking/bit_manipulate/PowerOfTwo.java
@@ -0,0 +1,16 @@
+package org.dng.code.grooking.bit_manipulate;
+
+public class PowerOfTwo {
+
+  /**
+   * Time: O(1)
+   * Space: O(1)
+   */
+  public boolean isPowerOfTwo(int n) {
+    // Any number power of two is bigger than 0
+    if (n <= 0)
+      return false;
+    return (n & (n - 1)) == 0;
+  }
+
+}
