ea4eb67c0f1e13062b91bc9f56f6c5a186e05648
duyntc <duyntc@vng.com.vn>
2023-03-13 17:42:54 +0700

    Hot fix unit test

--- code/src/main/java/org/dng/code/by_tag/depth_first_search/PossibleBipartition.java
+++ code/src/main/java/org/dng/code/by_tag/depth_first_search/PossibleBipartition.java
@@ -1,53 +1,60 @@
 package org.dng.code.by_tag.depth_first_search;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author duyntc 12/2022
  */
 public class PossibleBipartition {
-
-/**
- * @desc: https://leetcode.com/problems/possible-bipartition/
- */
-
-static public boolean possibleBipartition(int n, int[][] dislikes) {
 	
-	Set<Integer> list0 = new HashSet<>();
-	Set<Integer> list1 = new HashSet<>();
+	/**
+	 * @desc: https://leetcode.com/problems/possible-bipartition/
+	 */
 	
 	
-	for (int[] dislike : dislikes) {
-		int i0 = dislike[0];
-		int i1 = dislike[1];
+	static public boolean dfs(int node, int nodeColor, Map<Integer, List<Integer>> adj, int[] color) {
+		color[node] = nodeColor;
+		if (!adj.containsKey(node))
+			return true;
+		for (int neighbor : adj.get(node)) {
+			if (color[neighbor] == color[node])
+				return false;
+			if (color[neighbor] == -1) {
+				if (!dfs(neighbor, 1 - nodeColor, adj, color))
+					return false;
+			}
+		}
+		return true;
+	}
+	
+	static public boolean possibleBipartition(int n, int[][] dislikes) {
 		
-		if (list0.contains(i1)) {
-			list0.add(i1);
-			list1.add(i0);
-		} else if (list1.contains(i0)) {
-			list1.add(i0);
-			list0.add(i1);
-		} else {
-			list1.add(i1);
-			list0.add(i0);
+		// Build adjacency list
+		Map<Integer, List<Integer>> adj = new HashMap<>();
+		for (int[] edge : dislikes) {
+			int a = edge[0], b = edge[1];
+			adj.computeIfAbsent(a, value -> new ArrayList<>()).add(b);
+			adj.computeIfAbsent(b, value -> new ArrayList<>()).add(a);
 		}
+		int[] color = new int[n + 1];
+		Arrays.fill(color, -1);
+		
+		for (int i = 1; i <= n; i++) {
+			if (color[i] == -1) {
+				// For each pending component, run DFS.
+				if (!dfs(i, 0, adj, color))
+					// Return false, if there is conflict in the component.
+					return false;
+			}
+		}
+		return true;
 	}
 	
-	for (Integer i : list0) {
-		if (list1.contains(i)) {
-			return false;
-		}
+	public static void main(String[] args) {
+		
+		
+		System.out.println(possibleBipartition(3, new int[][]
+				{{1, 2}, {3, 4}, {5, 6}, {6, 7}, {8, 9}, {7, 8}}
+		));
 	}
-	
-	return true;
-}
-
-public static void main(String[] args) {
-	
-	
-	System.out.println(possibleBipartition(3, new int[][]
-													  {{1, 2}, {3, 4}, {5, 6}, {6, 7}, {8, 9}, {7, 8}}
-	));
-}
 }
